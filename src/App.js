3d4e613eacb82e07cf70c17c9c6ad033078acea6
komcal <kom.sujautra@gmail.com>
2017-11-14 16:42:28 +0700

    add bootstrap

--- src/App.js
+++ src/App.js
@@ -4,6 +4,13 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-4">3</div>
+            <div className="col-md-4">3</div>
+            <div className="col-md-4">3</div>
+          </div>
+        </div>
       </div>
     );
   }
