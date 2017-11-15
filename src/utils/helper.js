9d0a69d2a7ef206acda0cd85fa149a21a79f99ba
tidjungs <waewprach.s@ku.th>
2017-11-15 18:13:18 +0700

    add subnet and ip convertor and get network address

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -1,1 +1,31 @@
 export const plus = (x, y) => x + y;
+
+export const convertToSubnet = n => 
+  [0,8,16,24].map(i => 
+    parseInt(('1'.repeat(n) + '0'.repeat(32 - n)
+  ).substr(i, 8), 2)).join('.');
+
+
+const map = { 'ANY': 0, 'A': 7, 'B': 15, 'C': 23 }
+export const generateSubnetByClass = c => 
+  new Array(32 - map[c]).fill(0).map(
+    (number, index) => convertToSubnet(index+1+map[c]) + ' / ' + (index+1+map[c]))
+
+const encodeIP = ip => 
+  ip.split('.')
+  .map(n => '0'.repeat(8 - (+n).toString(2).length) + (+n).toString(2))
+  .join('')
+
+const decodeIP = ip => {
+  const buffer = new Array(4).fill(0);
+  return buffer.map((b, index) => parseInt(ip.substr(index*8, 8), 2)).join('.');
+}
+
+export const getNetWorkAddress = (ip, subnet) => {
+  const a = encodeIP(ip)
+    .split('')
+    .map((digit, index) => index < subnet ? digit : '0')
+    .join('')
+  return decodeIP(a);      
+}
+
--- src/utils/helper.test.js
+++ src/utils/helper.test.js
@@ -1,9 +1,51 @@
 import { expect } from 'chai';
-import { plus } from './helper';
+import { plus, convertToSubnet, generateSubnetByClass, getNetWorkAddress } from './helper';
 
 describe('test plus', () => {
   it('should plus number', () => {
     expect(plus(1, 2)).to.equal(3);
     expect(plus(0, 2)).to.equal(2);
+  })
+})
+
+describe('test convert subnet', () => {
+  it('should convert subnet', () => {
+    expect(convertToSubnet(1)).to.equal('128.0.0.0')
+    expect(convertToSubnet(2)).to.equal('192.0.0.0')
+    expect(convertToSubnet(3)).to.equal('224.0.0.0')
+    expect(convertToSubnet(16)).to.equal('255.255.0.0')
+    expect(convertToSubnet(24)).to.equal('255.255.255.0')
+    expect(convertToSubnet(32)).to.equal('255.255.255.255')    
+  })
+})
+
+
+describe('test generateSubnetByClass', () => {
+  it('should generateSubnetByClass', () => {
+    const expectValue = [
+      `${convertToSubnet(24)} / 24`,
+      `${convertToSubnet(25)} / 25`,
+      `${convertToSubnet(26)} / 26`,
+      `${convertToSubnet(27)} / 27`,
+      `${convertToSubnet(28)} / 28`,
+      `${convertToSubnet(29)} / 29`,
+      `${convertToSubnet(30)} / 30`,
+      `${convertToSubnet(31)} / 31`,
+      `${convertToSubnet(32)} / 32`,      
+    ];
+    const generateValue = generateSubnetByClass('C');
+    expectValue.forEach((value, index) => {
+      expect(generateValue[index]).to.equal(value)
+    })
+  })
+})
+
+describe('test getNetWorkAddress', () => {
+  it('should generate network address', () => {
+    expect(getNetWorkAddress('158.108.12.34', 1)).to.equal('128.0.0.0')    
+    expect(getNetWorkAddress('158.108.12.34', 8)).to.equal('158.0.0.0')        
+    expect(getNetWorkAddress('158.108.12.34', 16)).to.equal('158.108.0.0')    
+    expect(getNetWorkAddress('158.108.12.34', 24)).to.equal('158.108.12.0')
+    expect(getNetWorkAddress('158.108.12.34', 28)).to.equal('158.108.12.32')        
   })
 })
