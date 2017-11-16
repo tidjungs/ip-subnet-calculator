ed3dbcf0a2c9cde97b9b65336010ad35603cb369
tidjungs <waewprach.s@ku.th>
2017-11-16 22:51:48 +0700

    add grommet

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
 import React, { Component } from 'react';
+import 'grommet/scss/vanilla/index';
+import GrommetApp from 'grommet/components/App';
+import Box from 'grommet/components/Box';
+import Header from 'grommet/components/Header';
+import Footer from 'grommet/components/Footer';
+import Meter from 'grommet/components/Meter';
+import Title from 'grommet/components/Title';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="container">
-          <div className="row">
-            <div class="page-header">
-              <h1>IP Subnet Calculator</h1>
-            </div>
-          </div>
-        </div>
-      </div>
+      <GrommetApp>
+        <Header direction="row" justify="between" size="large"
+          pad={{ horizontal: 'medium' }}>
+          <Title>Grommet standalone</Title>
+        </Header>
+        <Box pad='medium'>
+          <Meter value={40} />
+        </Box>
+        <Footer primary={true} appCentered={true} direction="column"
+          align="center" pad="small" colorIndex="grey-1">
+          <p>
+            Build your ideas with <a href="http://grommet.io" target="_blank">Grommet</a>!
+          </p>
+        </Footer>
+      </GrommetApp>
     );
   }
 }
