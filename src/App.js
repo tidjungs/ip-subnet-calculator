2069a3a5bb9e0cc53c4e0257c72aeb07aefca642
tidjungs <waewprach.s@ku.th>
2017-11-17 00:26:33 +0700

    add selected subnet

--- src/App.js
+++ src/App.js
@@ -1,18 +1,70 @@
 import React, { Component } from 'react';
 import { 
   Input,
-  Icon
+  Icon,
+  Radio,
+  Select
 } from 'antd';
 
+import { generateSubnetByClass } from './utils/helper';
 import './App.css';
 
+const RadioGroup = Radio.Group;
+const Option = Select.Option;
+
 class App extends Component {
+  state = {
+    selectedClass: 'ANY',
+    selectledSubnet: null,    
+    subnets: generateSubnetByClass('ANY'),
+  }
+  
+  onClassChange = e => {
+    this.setState({
+      selectedClass: e.target.value,
+      subnets: generateSubnetByClass(e.target.value)
+    });
+  }
+
+  onSubnetChange = subnet => {
+    this.setState({
+      selectledSubnet: +subnet.slice(-2)
+    })
+  }
+
   render() {
+    const { selectedClass, selectledSubnet, subnets } = this.state;
+    console.log(selectledSubnet)
     return (
       <div className="container">
         <h1>IP Subnet Calcultor <Icon type="calculator" /></h1>
-        <label>IP Address</label>
-        <Input placeholder="IP Address" />        
+        <div className="group">
+          <label className="title">Network Class</label>
+          <RadioGroup onChange={this.onClassChange} value={selectedClass}>
+            <Radio value={'ANY'}>ANY</Radio>
+            <Radio value={'A'}>A</Radio>
+            <Radio value={'B'}>B</Radio>
+            <Radio value={'C'}>C</Radio>
+          </RadioGroup>
+        </div>
+        <div className="group">
+          <label className="title">Subnet</label>
+          <Select
+            showSearch
+            placeholder="Select IP"
+            onChange={this.onSubnetChange}
+          >
+            {
+              subnets.map(net => 
+                <Option key={net}>{net}</Option>
+              )
+            }
+          </Select>
+        </div>
+        <div className="group">
+          <label className="title">IP Address</label>
+          <Input placeholder="IP Address" />
+        </div>
       </div>
     );
   }
