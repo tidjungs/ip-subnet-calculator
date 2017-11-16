e0a7722a70157837c4641f9b21fc7ccdba332662
tidjungs <waewprach.s@ku.th>
2017-11-16 23:40:55 +0700

    add header and input

--- src/App.js
+++ src/App.js
@@ -1,30 +1,19 @@
 import React, { Component } from 'react';
-import 'grommet/scss/vanilla/index';
-import GrommetApp from 'grommet/components/App';
-import Box from 'grommet/components/Box';
-import Header from 'grommet/components/Header';
-import Footer from 'grommet/components/Footer';
-import Meter from 'grommet/components/Meter';
-import Title from 'grommet/components/Title';
+import { 
+  Input,
+  Icon
+} from 'antd';
+
+import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <GrommetApp>
-        <Header direction="row" justify="between" size="large"
-          pad={{ horizontal: 'medium' }}>
-          <Title>Grommet standalone</Title>
-        </Header>
-        <Box pad='medium'>
-          <Meter value={40} />
-        </Box>
-        <Footer primary={true} appCentered={true} direction="column"
-          align="center" pad="small" colorIndex="grey-1">
-          <p>
-            Build your ideas with <a href="http://grommet.io" target="_blank">Grommet</a>!
-          </p>
-        </Footer>
-      </GrommetApp>
+      <div className="container">
+        <h1>IP Subnet Calcultor <Icon type="calculator" /></h1>
+        <label>IP Address</label>
+        <Input placeholder="IP Address" />        
+      </div>
     );
   }
 }
